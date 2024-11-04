bd2c11d7351252fc10cf163becef88a9e7721ca4
rich29r <120351130+rich29r@users.noreply.github.com>
2024-11-04 15:48:17 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 
 echo "Hi ${USER} !!!"
 
-if ! command -v python3 &>/dev/null || command -v pip3 &>/dev/null; then
+if ! command -v python3 &>/dev/null || ! command -v pip3 &>/dev/null; then
     read -p "Quieres instalar python3 y pip3? Y/n: " sn
     if [[ "${sn}" == "y" || "${sn}" == "Y" ]]; then
         apt-get install -y python3
